bb8b9b84cd168e550e43c92b55c8f2a699aa7462
Deathkuroneko <deathkuroneko@hotmail.com>
2026-05-05 22:22:29 -0500

    Delete Taller2 directory

--- Taller2/src/main/java/frame/FiltroConfigurable.java
+++
@@ -1,9 +0,0 @@
-package frame;
-import javax.swing.JSlider;
-import javax.swing.JLabel;
-
-public interface FiltroConfigurable {
-    void configurarSlider(JSlider slider, JLabel lblIntensidad);
-    void aplicarFiltro(int valorSlider);
-    String getNombreFiltro();
-}
